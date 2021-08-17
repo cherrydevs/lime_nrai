8c2ad32e622b0524304be317bd2554099752e691
MatteeCodes <46092609+MatteeCodes@users.noreply.github.com>
2021-08-17 16:59:04 -0500

    starting to work on a neural network example and maybe a real one soon

--- src/lib.rs
+++ src/lib.rs
@@ -4,19 +4,25 @@ mod delta;
 #[cfg(test)]
 mod tests {
     use crate::delta::*;
+    use futures::executor::block_on;
     #[test]
     fn test() {
         let mut instance = Instance::new();
         instance.memory.push(Memory::Text(String::from("lime")));
         instance.reference.push(Ref::Text(String::from("a green bitter fruit")));
         instance.similar.push(Ref::Similar(vec![Ref::Text(String::from("lemon"))]));
+        for x in 0..4 {
+            // they all have the same weight, weight translation coming soon
+            let node = neraul_node(&instance, String::from("lemon"));
+            block_on(node);
+        }
+    }
 
-
+    async fn neraul_node(instance: &Instance, matcher: String) {
         // get the lime reference
-        let index = match_memory(&instance.memory, String::from("lime"));
-        println!("object: {0:?}, description: {1:?}, similar to: {2:?}", instance.memory[index], instance.reference[index], instance.similar[index]);
 
-        let indexsim = match_similar(&instance.similar, String::from("lemon"));
+        let indexsim = match_similar(&instance.similar, String::from(matcher));
+
         // print out things that match with lemon in similar state
         println!("what it matched: {0:?},matching string: {1:?}", instance.memory[indexsim], instance.similar[indexsim]);
     }
